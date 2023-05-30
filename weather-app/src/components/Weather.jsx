308ec06c3da296be9ba350f1fabe94d9e3ba0734
jacktham96 <62689233+jacktham96@users.noreply.github.com>
2023-05-30 14:34:09 +0800

    Update Weather APP

--- weather-app/src/components/Weather.jsx
+++ weather-app/src/components/Weather.jsx
@@ -1,8 +1,7 @@
 import React, {useEffect,useState} from 'react'
 import { apiKey , algoliaApiKey ,appID } from '../environment' 
-import AlgoliaPlaces from 'algolia-places-react';  //https://www.npmjs.com/package/algolia-places-react
 import CurrentWeather from './CurrentWeather'
-
+import Search from './Search';
 
 
 const Weather = () => {
@@ -45,25 +44,33 @@ const Weather = () => {
     }, [url])
 
 
+    const handleonSearchChange = (searchData) => {
+        const [lat, lon] = searchData.value.split("")
+        const currentWeatherFetch = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
+        console.log(searchData);
+
+        Promise.all([currentWeatherFetch])
+            .then(async(response)=>{
+                const weatherResponse = await response[0].json()
+
+                setCurrentWeather({city: searchData.label, ...weatherResponse})
+            })
+            .catch((error)=>console.log(error))
+    }
+
   return (
     <div className='weather-container mt-32'>
-        <div className='searchbar'>
-            <AlgoliaPlaces
-                placeholder='Write an address here'
-                onChange = {({suggestion}) => 
-                setUrl(`https://api.openweathermap.org/data/2.5/weather?lat=${suggestion.latlng.lat}&lon=${suggestion.latlng.lng}&appid=${apiKey}&units=metric`)
-                }
 
-                options={{
-                    appID,
-                    apiKey: algoliaApiKey,
-                    aroundLatLngViaIP: false // disable the extra search/boost around the source IP
-                  }}
-
+        <div className='searchbar2'>
+            <Search 
+                onSearchChange = {handleonSearchChange}
             />
         </div>
+
+
+
         <div className='currentweather mt-16'>
-            <CurrentWeather weather={currentWeather}/>
+            {currentWeather && <CurrentWeather weather={currentWeather}/>}
         </div>
     </div>
   )
--- weather-app/src/environment.js
+++ weather-app/src/environment.js
@@ -1,6 +1,18 @@
 // OpenWeather API KEY
-export const apiKey = '8ca03ddaa66e6546366c379089c6f41f';
+export const apiKey = '3ef0942c2a2f131b9eea0c8fc734c0e7';
 
 // algolia API KEY   https://www.algolia.com/account/api-keys/all?applicationId=4P2ZZ5BUCW
 export const appID = '4P2ZZ5BUCW'
 export const algoliaApiKey = 'f93bca4688c4ab0a1d8db51ecd641b66'
+
+
+// GeoDB API Key and information
+
+export const GeoAPIUrl = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const GeoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7d79bf757amsh0d7a59190ebc989p11f60ejsn356768a3d811',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
